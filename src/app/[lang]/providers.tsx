6fd1109312fe82fdf0f63affc59652f440ccd87d
yukifury <forlorn128@gmail.com>
2026-06-02 00:17:00 +0100

    refactor(query): reuse shared query client

--- src/app/[lang]/providers.tsx
+++ src/app/[lang]/providers.tsx
@@ -1,33 +1,22 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { SidebarProvider } from '@/context/SidebarContext';
 import { Toaster } from 'react-hot-toast';
-import { useState } from 'react';
 import { I18nProvider } from '@/shared/providers/i18n/i18n.provider';
-import { Locale } from '@/app/[lang]/dictionaries';
+import type { Locale } from '@/app/[lang]/dictionaries';
+import BasicQueryClient from '@/shared/api/core/basic-query.client';
 
 export interface ProvidersProps {
   children?: React.ReactNode;
-  dictionary: any;
+  dictionary: unknown;
   lang: Locale;
 }
 
 export function Providers({ children, dictionary, lang }: ProvidersProps) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: false,
-          },
-        },
-      }),
-  );
-
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={BasicQueryClient}>
       <I18nProvider lang={lang} dictionary={dictionary}>
         <ThemeProvider>
           <Toaster containerClassName="z-99" position="bottom-right" />
